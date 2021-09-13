0983e969c3247775755c863d5d431b5aa3245180
gabrielchanchi <56714173+gabrielchanchi@users.noreply.github.com>
2021-09-13 13:51:16 -0500

    Update funciones.py
    Se agregó la función para calcular el volumen del paralelepípedo

--- funciones.py
+++ funciones.py
@@ -1,6 +1,11 @@
 def calcular_area_cuadrado (lado):
 	area=lado*lado
 	return area
+
+def calcular_volumen_paralelepipedo(base,altura, alto):
+	volumen=base*altura*alto
+	return volumen
+	
 	
 #print(f"El area del cuadrado es {calcular_area_cuadrado(4)}")
 
