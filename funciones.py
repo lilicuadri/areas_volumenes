172144560c55473212b85461822d05e5ed446887
gabrielchanchi <56714173+gabrielchanchi@users.noreply.github.com>
2021-09-13 13:38:46 -0500

    Add files via upload
    Se agrega la función para calcular el área del cuadrado

---
+++ funciones.py
@@ -0,0 +1,9 @@
+def calcular_area_cuadrado (lado):
+	area=lado*lado
+	return area
+	
+	
+#print(f"El area del cuadrado es {calcular_area_cuadrado(4)}")
+
+	 
+	
---
+++ principal.py
@@ -0,0 +1,4 @@
+from funciones import *
+
+
+print(f"El area del cuadrado es {calcular_area_cuadrado(5)}")
